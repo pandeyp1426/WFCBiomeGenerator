dede0b37912309f51becdb27f53d03dab56102f3
Jacob Way <jacobbway1997@gmail.com>
2026-04-23 13:07:51 -0500

    created mapRenderer
    this does the stuff to the 2d vector holding rectangles

---
+++ Graphics/mapRenderer.cpp
@@ -0,0 +1,46 @@
+#include "../Graphics/mapRenderer.hpp"
+#include "../BiomeClasses/map.hpp"
+#include "../BiomeClasses/cell.hpp"
+#include "../BiomeClasses/Biome.hpp"
+
+MapRenderer::MapRenderer(int numRows, int numCols, std::vector<std::tuple<int,int,char>> userDefinedCells) : 
+cellMap(numRows, numCols, userDefinedCells){
+    this->mapOfCells = cellMap.getMap();
+    buildDisplayMap();
+}
+
+void MapRenderer::buildDisplayMap(){
+    displayMap.resize(mapOfCells.size(), std::vector<sf::RectangleShape>(mapOfCells.at(0).size()));
+
+    for(int row = 0; row < mapOfCells.size(); row++){
+        for(int col = 0; col < mapOfCells.at(row).size(); col++){
+            Cell* curCell = cellMap.getCell(row, col);
+
+            sf::RectangleShape newRect;
+            newRect.setSize(sf::Vector2f(cellSize,cellSize));
+            newRect.setOutlineColor(sf::Color::Black);
+            newRect.setPosition({static_cast<float>(row) * cellSize, static_cast<float>(col) * cellSize});
+            newRect.setFillColor(biomeToColor(curCell->getBiomeOfCell()));
+            displayMap.at(row).at(col) = newRect;
+        }
+    }
+}
+
+void MapRenderer::updateDisplayMap(int row, int col, sf::RenderWindow &window){
+    Cell* curCell = cellMap.getCell(row, col);
+    displayMap.at(row).at(col).setFillColor(biomeToColor(curCell->getBiomeOfCell()));
+    window.draw(displayMap.at(row).at(col));
+}
+
+void MapRenderer::initialMapDraw(sf::RenderWindow &window){
+    for(int row = 0; row < displayMap.size(); row++){
+        for(int col = 0; col < displayMap.at(row).size(); col++){
+            window.draw(displayMap.at(row).at(col));
+        }
+    }
+}
+
+void MapRenderer::generateMap(sf::RenderWindow &window){
+    std::pair<int, int> curCoords = cellMap.generateMap();
+    updateDisplayMap(curCoords.first, curCoords.second, window);
+}
---
+++ Graphics/mapRenderer.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "../BiomeClasses/map.hpp"
+#include "../BiomeClasses/Biome.hpp"
+
+class Map;
+class Cell;
+
+class MapRenderer{
+    private:
+        std::vector<std::vector<sf::RectangleShape>> displayMap;
+        Map cellMap;
+        std::vector<std::vector<Cell*>> mapOfCells;
+        sf::RenderWindow currentWindow;
+        int cellSize = 4;
+        
+        public:
+        MapRenderer(int numRows, int numCols, std::vector<std::tuple<int,int,char>> userDefinedCells = {});
+        void buildDisplayMap();
+        void updateDisplayMap(int row, int col, sf::RenderWindow &window);
+        void initialMapDraw(sf::RenderWindow &window);
+        std::vector<std::vector<Cell*>> &getCellMap(){ return this->mapOfCells; }
+        void generateMap(sf::RenderWindow &window);
+};
