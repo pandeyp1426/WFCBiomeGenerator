4e4b3edb025990b12c65e59ad4afd394042f4c61
Jacob Way <jacobbway1997@gmail.com>
2026-04-23 13:06:16 -0500

    biome update
    changed biomeToColor to accept a char and made the case a char as well

--- BiomeClasses/Biome.hpp
+++ BiomeClasses/Biome.hpp
@@ -11,16 +11,16 @@ enum class Biome
     P, // Plains
 };
 
-inline sf::Color biomeToColor(Biome biome)
+inline sf::Color biomeToColor(char biome)
 {
     switch (biome)
     {
-        case Biome::C:  return sf::Color(240, 230, 140);
-        case Biome::D:  return sf::Color(204, 173, 96);
-        case Biome::F:  return sf::Color(34, 139, 34);
-        case Biome::M:  return sf::Color(128, 128, 128);
-        case Biome::O:  return sf::Color(0, 105, 148);
-        case Biome::P:  return sf::Color(50, 205, 50);
+        case 'C':  return sf::Color(240, 230, 140);
+        case 'D':  return sf::Color(204, 173, 96);
+        case 'F':  return sf::Color(34, 139, 34);
+        case 'M':  return sf::Color(128, 128, 128);
+        case 'O':  return sf::Color(0, 105, 148);
+        case 'P':  return sf::Color(50, 205, 50);
         default:        return sf::Color::White;
     }
 }
