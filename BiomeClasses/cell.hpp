c10842fe6d712907ef02ff6bab3faa93089118ac
Jacob Way <jacobbway1997@gmail.com>
2026-04-22 21:32:43 -0500

    Updates to cell
    -added a flag that shows if a cell is set or not
    -cell entropy is intialized to 0.0 now
    -removed updateOptions it was redudant with setCurrentOptions
    -set current options had a return type it shouldn't have though
    -added getters/setters for the isCellSet flag

--- BiomeClasses/cell.hpp
+++ BiomeClasses/cell.hpp
@@ -5,6 +5,7 @@
 class Cell {
     private:
         bool isSetByUser = false; // if this is true the cell cant change.
+        bool isCellSet = false;
 
         std::set<char> currentOptions = {'O', 'C', 'P'}; // O = Ocean, C = Coast, P = Plains // true means its available
         
@@ -12,7 +13,7 @@ class Cell {
         
         int numOfRemainingOptions = currentOptions.size(); // just the amount of options in the unordered map // can just be the entropy of our cell
         
-        double cellEntropy; // will be this [this->numOfRemainingOptions + (numOfRemainingOptions of surrounding 8 cells)] / 9
+        double cellEntropy = 0.0; // will be this [this->numOfRemainingOptions + (numOfRemainingOptions of surrounding 8 cells)] / 9
         
         std::vector<std::tuple<int, int, Cell*>> surroundingCells;
         
@@ -21,8 +22,6 @@ class Cell {
     public:
         Cell(int cellRow, int cellCol);
         Cell(bool isSetByUser, char biomeOfCell, int cellRow, int cellCol);
-
-        void updateOptions(char key, bool updateBool);
         
         bool setBiomeOfCell(char chosenBiome);
         double getCellEntropy(){ return cellEntropy; }
@@ -30,7 +29,10 @@ class Cell {
         std::vector<std::tuple<int, int, Cell*>>& getSurroundCellVect() { return surroundingCells; }
         bool getIsSetByUser(){ return isSetByUser; }
         std::set<char>& getCurrentOptions(){ return currentOptions; }
-        std::set<char> setCurrentOptions(std::set<char> newBiomeOptions){ this->getCurrentOptions() = newBiomeOptions; }
+        void setCurrentOptions(std::set<char> newBiomeOptions){ this->getCurrentOptions() = newBiomeOptions; }
         int getNumberOfRemainingOptions(){ return currentOptions.size(); }
         char getBiomeOfCell(){ return biomeOfCell; }
+        std::pair<int,int> getCellCoords(){ return {this->cellRow, this->cellCol}; }
+        bool getIsCellSet(){ return this->isCellSet; }
+        void setIsCellSet(bool isSet){ this->isCellSet = isSet; }
 };
