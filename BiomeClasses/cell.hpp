9c676a9e249a5525b9efc9e6298dd8641d5d9fa4
Jacob Way <jacobbway1997@gmail.com>
2026-04-24 14:58:35 -0500

    added my version of current options

--- BiomeClasses/cell.hpp
+++ BiomeClasses/cell.hpp
@@ -5,6 +5,7 @@ class Cell {
     private:
         bool isSetByUser = false; // if this is true the cell cant change.
         std::unordered_map<char, bool> currentOptions = {{'O', true}, {'C', true}, {'P', true}}; // O = Ocean, C = Coast, P = Plains // true means its available
+        // std::set<char> currentOptions = {'C', 'D', 'F', 'M', 'O', 'P'};
         char biomeOfCell = '\0'; // this is the super position basically since it has no choice.
         int numOfRemainingOptions = currentOptions.bucket_count(); // just the amount of options in the unordered map // can just be the entropy of our cell
         double cellEntropy; // will be this [this->numOfRemainingOptions + (numOfRemainingOptions of surrounding 8 cells)] / 9
