38c0b42e00dfde2c865ccb1d7a90c68b2eff8e1e
Jacob Way <jacobbway1997@gmail.com>
2026-04-23 20:45:01 -0500

    added new colors

--- BiomeClasses/Biome.hpp
+++ BiomeClasses/Biome.hpp
@@ -1,26 +1,18 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-enum class Biome
-{
-    C, // Coast
-    D, // Desert
-    F, // Forest
-    M, // Mountains
-    O, // Ocean
-    P, // Plains
-};
-
 inline sf::Color biomeToColor(char biome)
 {
     switch (biome)
     {
-        case 'C':  return sf::Color(240, 230, 140);
-        case 'D':  return sf::Color(204, 173, 96);
-        case 'F':  return sf::Color(34, 139, 34);
-        case 'M':  return sf::Color(128, 128, 128);
-        case 'O':  return sf::Color(0, 105, 148);
-        case 'P':  return sf::Color(50, 205, 50);
+        case 'C':  return sf::Color(240, 230, 140); // Coast
+        case 'D':  return sf::Color(181, 66, 51);   // Desert
+        case 'F':  return sf::Color(34, 139, 34);   // Forest
+        case 'I':  return sf::Color(16, 38, 17);    // Deep Forest
+        case 'M':  return sf::Color(128, 128, 128); // Mountains
+        case 'O':  return sf::Color(0, 105, 148);   // Ocean
+        case 'P':  return sf::Color(66, 26, 4);   // Plains
+        case 'S':  return sf::Color(30, 32, 117);   // Deep Ocean
         default:        return sf::Color::White;
     }
 }
