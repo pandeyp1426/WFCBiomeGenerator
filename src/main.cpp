c310430e5626d82d31ac7362649b2f1408f88532
Jacob Way <jacobbway1997@gmail.com>
2026-04-27 10:12:12 -0500

    updates to main
    -render window is actually using the windowHeight/Width variables now.
    -added the OpenSans.ttf code to make sure the file is found
    -added button creating and drawing tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <SFML/Graphics.hpp>
 #include "../Graphics/mapRenderer.hpp"
+#include "../Graphics/button.hpp"
 
 int main()
 {
 	const auto mapW = 100;
 	const auto mapH = 100;
 	const int cellSize = 4;
-	int windowWidth = mapW * 4 + 100;
+	int windowWidth = mapW * 4 + 200;
 	int windowHeight = mapH * 4 + 8;
 
-	
-	sf::RenderWindow window( sf::VideoMode( { mapW * cellSize + 100, mapH * cellSize + 8 } ), "Map Generator", sf::Style::Titlebar | sf::Style::Close | sf::Style::Resize);
+	sf::Font uiFont;
+	if(!uiFont.openFromFile("../../../../Graphics/fonts/OpenSans.ttf")){
+		throw std::runtime_error("Failed to load font");
+	}
+
+	std::string randomizeString = "Randomize";
+
+	Button randomSeedButton(windowWidth - 180, 10, 150, 40, randomizeString, uiFont);
+
+	sf::RenderWindow window( sf::VideoMode( { static_cast<unsigned int>(windowWidth), static_cast<unsigned int>(windowHeight) } ), "Map Generator", sf::Style::Titlebar | sf::Style::Close | sf::Style::Resize);
 	std::vector<std::tuple<int, int, char>> userDefinedCellsTest = { {5, 5, 'O'} };
+	std::vector<std::tuple<int,int,char>> userTest = {{50, 50, 'F'}};
 
 
 	MapRenderer newMap(mapW, mapH, userDefinedCellsTest);
@@ -23,15 +33,16 @@ int main()
 		{
 			if ( event->is<sf::Event::Closed>() )
 				window.close();
-		}
 
-		for(int i = 0; i < 20; i++){
-			newMap.generateMap(window);
+			for(int i = 0; i < 20; i++){
+				newMap.generateMap(window);
+			}
+			
+			window.clear(sf::Color(173, 156, 154));
+			newMap.initialMapDraw(window);
+			randomSeedButton.drawButton(window);
+			window.display();
 		}
-		
-		window.clear(sf::Color(173, 156, 154));
-		newMap.initialMapDraw(window);
-		window.display();
 	}
 
 }
