67b053ef7a513bd836b3adf0f9fe42928a9dc7f0
Jacob Way <jacobbway1997@gmail.com>
2026-05-06 10:03:09 -0500

    Update to biome
    Just fixed some spacing because some of the line in the switch statement had different tab amounts

--- BiomeClasses/Biome.hpp
+++ BiomeClasses/Biome.hpp
@@ -18,9 +18,9 @@ inline sf::Color biomeToColor(Biome biome)
     case Biome::Ocean:     return sf::Color(0, 105, 148);
     case Biome::Coast:     return sf::Color(240, 230, 140);
     case Biome::Plains:    return sf::Color(50, 205, 50);
-   case Biome::Forest:    return sf::Color(34, 139, 34);
+    case Biome::Forest:    return sf::Color(34, 139, 34);
     case Biome::Swamp:     return sf::Color(47, 79, 79);
-   case Biome::Mountains: return sf::Color(128, 128, 128);
+    case Biome::Mountains: return sf::Color(128, 128, 128);
     default:               return sf::Color::White;
     }
 }
